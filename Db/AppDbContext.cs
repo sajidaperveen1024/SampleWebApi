c31f40a39c13572db4bfb20fdd41793228c89068
sajidaperveen1024 <sajidaperveen1024@gmail.com>
2025-09-20 02:39:06 +0100

    Add project files.

---
+++ Db/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleWebApi.Model;
+using System.Collections.Generic;
+
+namespace SampleWebApi.Db
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
