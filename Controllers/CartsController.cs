ef4ec9daf773b0e6c186bd70eed857306431b692
sajidaperveen1024 <sajidaperveen1024@gmail.com>
2025-10-12 21:11:54 +0100

    changes and build API

--- Controllers/CartsController.cs
+++ Controllers/CartsController.cs
@@ -7,7 +7,16 @@ namespace SampleWebApi.Controllers
     [ApiController]
     public class CartsController : ControllerBase
     {
+        public IActionResult Index()
+        {
+            return Ok("CartsController is working!");
+        }
 
+        public Task<IActionResult> GetCartItems(int cartId)
+        {
+            // Logic to retrieve cart items by cartId
+            return Task.FromResult<IActionResult>(Ok(new { CartId = cartId, Items = new string[] { "Item1", "Item2" } }));
+        }   
 
     }
 }
